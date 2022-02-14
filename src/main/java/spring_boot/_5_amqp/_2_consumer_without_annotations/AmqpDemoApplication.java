509d1b5377e2c1099f00c75b2dc84fbd070766b5
Abdelrahman_Attia <abdelrahmanattya94@gmail.com>
2022-02-14 22:15:00 +0200

    added some examples about rabbitMQ

---
+++ src/main/java/spring_boot/_5_amqp/_2_consumer_without_annotations/AmqpDemoApplication.java
@@ -0,0 +1,266 @@
+package spring_boot._5_amqp._2_consumer_without_annotations;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageListener;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Parameter;
+import java.util.stream.IntStream;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SpringBootApplication
+public class AmqpDemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AmqpDemoApplication.class, args);
+	}
+
+	// exchange is an empty string since there is no property apress.amqp.exchange in the properties file
+	// routing key is similar to the queue name since we are using the default exchange type. this might change according to our needs.
+	@Bean
+	CommandLineRunner simple(@Value("${apress.amqp.exchange:}") String exchange,
+			@Value("${apress.amqp.queue}") String routingKey, Producer producer) {
+		return args -> {
+			producer.sendMessage(exchange, routingKey, "HELLO AMQP!");
+		};
+	}
+}
+
+//======================================  Producer ==========================================//
+
+@Component
+class Producer {
+	private RabbitTemplate template;
+
+	@Autowired
+	public Producer(RabbitTemplate template) {
+		this.template = template;
+	}
+
+	public void sendMessage(String exchange, String routingKey, String message) {
+		this.template.convertAndSend(exchange, routingKey, message);
+	}
+}
+
+//======================================  Consumer ==========================================//
+
+@Component
+class Consumer implements MessageListener {
+
+	@Override
+	public void onMessage(Message message) {
+		
+
+	}
+}
+
+//======================================  Configuration =========================================//
+
+@Configuration
+@EnableConfigurationProperties(AMQPProperties.class)
+class AMQPConfig {
+	
+	//TODO: is this bean really needed ?? if we removed it, we still can send and
+	// listen to the message
+	@Bean
+	public Queue queue(@Value("${apress.amqp.queue}") String queueName) {
+		return new Queue(queueName, false);
+	}
+	
+	// this configuration bean is needed for the consumer
+	@Bean
+	public SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, MessageListener consumer,
+			@Value("${apress.amqp.queue}") String queueName) {
+		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.setQueueNames(queueName);
+		container.setMessageListener(consumer);
+		return container;
+	}
+
+}
+
+//======================================  Properties =========================================//
+
+@ConfigurationProperties(prefix = "apress.amqp") // this will declare a custom properties that will have
+													// the prefix apress.amqp
+class AMQPProperties {
+
+	private String queue;
+	private String replyQueue;
+	private String replyExchangeQueue;
+	private String exchange = "";
+	private String errorQueue;
+	private String errorExchange = "";
+	private String rateQueue;
+	private String rateExchange = "";
+	private String errorRoutingKey = "error";
+
+	public String getQueue() {
+		return queue;
+	}
+
+	public void setQueue(String queue) {
+		this.queue = queue;
+	}
+
+	public String getReplyQueue() {
+		return replyQueue;
+	}
+
+	public void setReplyQueue(String replyQueue) {
+		this.replyQueue = replyQueue;
+	}
+
+	public String getReplyExchangeQueue() {
+		return replyExchangeQueue;
+	}
+
+	public void setReplyExchangeQueue(String replyExchangeQueue) {
+		this.replyExchangeQueue = replyExchangeQueue;
+	}
+
+	public String getRateQueue() {
+		return rateQueue;
+	}
+
+	public void setRateQueue(String rateQueue) {
+		this.rateQueue = rateQueue;
+	}
+
+	public String getExchange() {
+		return exchange;
+	}
+
+	public void setExchange(String exchange) {
+		this.exchange = exchange;
+	}
+
+	public String getRateExchange() {
+		return rateExchange;
+	}
+
+	public void setRateExchange(String rateExchange) {
+		this.rateExchange = rateExchange;
+	}
+
+	public String getErrorQueue() {
+		return errorQueue;
+	}
+
+	public void setErrorQueue(String errorQueue) {
+		this.errorQueue = errorQueue;
+	}
+
+	public String getErrorExchange() {
+		return errorExchange;
+	}
+
+	public void setErrorExchange(String errorExchange) {
+		this.errorExchange = errorExchange;
+	}
+
+	public String getErrorRoutingKey() {
+		return errorRoutingKey;
+	}
+
+	public void setErrorRoutingKey(String errorRoutingKey) {
+		this.errorRoutingKey = errorRoutingKey;
+	}
+
+}
+
+//======================================  Auditing =========================================//
+
+@Aspect
+@Component
+class AMQPAudit {
+	private static final String DASH_LINE = "===================================";
+	private static final String NEXT_LINE = "\n";
+	private static final Logger log = LoggerFactory.getLogger("AMQPAudit");
+
+	@Pointcut("execution(* spring_boot._5_amqp._2_consumer_without_annotations.*.*(..))")
+	public void logAMQP() {
+	};
+
+	@Around("logAMQP()")
+	public Object amqpAudit(ProceedingJoinPoint pjp) throws Throwable {
+		StringBuilder builder = new StringBuilder(NEXT_LINE);
+		printBefore(builder, pjp);
+
+		// Method Execution
+		Object object = pjp.proceed(pjp.getArgs());
+
+		printAfter(builder, object);
+		log.info(builder.toString());
+
+		return object;
+	}
+
+	@Before("execution(* com.apress.messaging.listener.*.*(..))")
+	public void auditListeners(JoinPoint joinPoint) {
+		StringBuilder builder = new StringBuilder(NEXT_LINE);
+		printBefore(builder, joinPoint);
+		builder.append(NEXT_LINE);
+		builder.append(DASH_LINE);
+		log.info(builder.toString());
+	}
+
+	private void printBefore(StringBuilder builder, JoinPoint jp) {
+		Object[] args = jp.getArgs();
+		Parameter[] parameters = ((MethodSignature) jp.getSignature()).getMethod().getParameters();
+
+		builder.append(DASH_LINE);
+		builder.append(NEXT_LINE);
+		builder.append("[BEFORE]");
+		builder.append(NEXT_LINE);
+		builder.append(" Class: ");
+		builder.append(jp.getTarget().getClass().getName());
+		builder.append(NEXT_LINE);
+		builder.append("Method: ");
+		builder.append(jp.getSignature().getName());
+		builder.append(NEXT_LINE);
+		builder.append("Params: ");
+		builder.append(NEXT_LINE);
+
+		if (null != args && null != parameters) {
+			IntStream.range(0, args.length).forEach(index -> {
+				builder.append("> ");
+				builder.append(parameters[index].getName());
+				builder.append(": ");
+				builder.append(args[index]);
+				builder.append(NEXT_LINE);
+			});
+		}
+	}
+
+	private void printAfter(StringBuilder builder, Object object) {
+		builder.append(NEXT_LINE);
+		builder.append("[AFTER]");
+		builder.append(NEXT_LINE);
+		builder.append("Return: ");
+		builder.append(object == null ? "void/null" : object);
+		builder.append(NEXT_LINE);
+		builder.append(DASH_LINE);
+	}
+}
